21ca463b40cad0f89050ec1b0f34a6a3484cfdac
Nurlan <nd.dz@yandex.ru>
2021-01-20 12:29:36 +0400

    Show info block

---
+++ src/js/modules/showInfo.js
@@ -0,0 +1,17 @@
+export default class ShowInfo {
+   constructor(triggers) {
+      this.btns = document.querySelectorAll(triggers);
+
+   }
+
+   init() {
+      this.btns.forEach(btn => {
+         btn.addEventListener('click', () => {
+            const sibling = btn.closest('.module__info-show').nextElementSibling;
+            console.log(sibling);
+            sibling.classList.toggle('msg');
+            sibling.style.marginTop = '20px';
+         });
+      });
+   }
+}
